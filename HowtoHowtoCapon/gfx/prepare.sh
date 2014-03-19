bb2b65d8e19628e991d350fa53d3f5a6e4f95c74
Jo Inge Buskenes <joibu@ifi.uio.no>
2014-03-19 02:18:35 +0100

    la2pdf commit - 2014:03:19	02:18:35	(Wed19.Mar)

--- HowtoHowtoCapon/gfx/prepare.sh
+++ HowtoHowtoCapon/gfx/prepare.sh
@@ -7,7 +7,7 @@ cp mvdr_build_R.eps buske4.eps
 cp buildR-breakdown.eps buske5.eps
 cp mvdr_implementation.eps buske6.eps
 cp plot_holmengraa_L16_Navg1.eps buske7.eps
-cp benchmark.eps buske8.eps
-cp code_assessment.eps buske9.eps
+cp code_assessment.eps buske8.eps
+cp benchmark.eps buske9.eps
 cp code_assess_flops.eps buske10.eps
 cp benchmark_boston.eps buske11.eps
